75521c5bb70e5960a671dbec9dbf548b947cbfc2
omega387 <ayush.yj2003gupta@gmail.com>
2025-10-04 11:03:19 +0530

    Stack changes

---
+++ StackQuestions/QueueUsingStack.java
@@ -0,0 +1,15 @@
+package Stack;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+    public void insert(int[] queue){
+        int[] S1 = new int[queue.length];
+        int[] S2 = new int[queue.length];
+
+    }
+
+    public static void main(String[] args) {
+        Stack<>
+    }
+}
