53e01dad42b9f80a3abec67cd2f54c943e621fe9
omega387 <ayush.yj2003gupta@gmail.com>
2025-10-04 11:05:47 +0530

    modifications

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,4 +1,10 @@
 package LinkedList;
 
 public class Node {
+    Node next;
+    int val;
+    Node(int val){
+        this.next = null;
+        this.val = val;
+    }
 }
--- StackQuestions/QueueUsingStack.java
+++ StackQuestions/QueueUsingStack.java
@@ -1,15 +1,53 @@
-package Stack;
+package StackQuestions;
 
 import java.util.Stack;
 
 public class QueueUsingStack {
-    public void insert(int[] queue){
-        int[] S1 = new int[queue.length];
-        int[] S2 = new int[queue.length];
 
+Stack<Integer> S1 = new Stack<>();
+    public void enqueue(int ele){
+        S1.push(ele);
+    }
+    public void printqueue(){
+        Stack<Integer> temp = new Stack<>();
+        for (int i : S1){
+            temp.push(i);
+        }
+
+        while (!temp.isEmpty()){
+            System.out.print(temp.peek()+ " ");
+            temp.pop();
+
+        }
+        System.out.println();
+    }
+
+    public void dequeue(){
+        Stack<Integer> S = new Stack<>();
+        while(!S1.isEmpty()){
+            S.push(S1.peek());
+            S1.pop();
+        }
+        S.pop();
+        while(!S.isEmpty()){
+            S1.push(S.peek());
+            S.pop();
+        }
     }
 
     public static void main(String[] args) {
-        Stack<>
+        QueueUsingStack Q1 = new QueueUsingStack();
+        Q1.enqueue(23);
+        Q1.enqueue(44);
+        Q1.enqueue(345);
+        Q1.enqueue(435);
+        Q1.enqueue(57);
+        Q1.dequeue();
+        Q1.printqueue();
+        Q1.enqueue(87);
+        Q1.dequeue();
+        Q1.printqueue();
+
+
     }
 }
