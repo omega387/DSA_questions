fb48b097b3572417c87a47486714a4ad6b37ef5d
omega387 <ayush.yj2003gupta@gmail.com>
2025-10-11 10:59:09 +0530

    modified graph

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,4 +1,31 @@
 package Graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
+    private Map<Integer, List<Integer>> adjlist;
+
+    public Graph(){
+        adjlist = new HashMap<>();
+    }
+
+    public void advertex(int ver){
+        adjlist.putIfAbsent(ver, new ArrayList<>());
+    }
+
+    public void addedge(int src,int dest){
+        adjlist.putIfAbsent(src, new ArrayList<>());
+        adjlist.putIfAbsent(dest,new ArrayList<>());
+        adjlist.get(src).add(dest);
+        adjlist.get(dest).add(src);
+    }
+
+    public void printGraph() {
+        for (var vertex : adjlist.keySet()) {
+            System.out.println(vertex + " -> " + adjlist.get(vertex));
+        }
+    }
 }
--- Graph/Main.java
+++ Graph/Main.java
@@ -1,4 +1,11 @@
 package Graph;
 
 public class Main {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.advertex(2);
+        g.addedge(2,3);
+        g.addedge(2,4);
+        g.printGraph();
+    }
 }
