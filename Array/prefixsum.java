1fe97bfd98baa3a0e79a7d8ab007f06fa3b60c2f
omega387 <ayush.yj2003gupta@gmail.com>
2025-10-18 11:03:04 +0530

    reworked prefixsum

--- Array/prefixsum.java
+++ Array/prefixsum.java
@@ -3,21 +3,18 @@ package Array;
 public class prefixsum {
     public static int[] presum(int[] arr ){
         int[] pre = new int[arr.length];
-        for (int i = 0; i < arr.length ; i++){
-            if(i==0){
-                pre[i]= arr[i];
-            }
-            else{
+        pre[0] = arr[0];
+        for (int i = 1; i < arr.length ; i++){
                 pre[i] = arr[i] + pre[i-1];
-            }
         }
         return pre;
     }
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7};
         int[] pre = presum(arr);
-        for(int i : pre){
+        for(int i = 0; i < pre.length - 1 ; i++ ){
             System.out.print(i + ",");
         }
+        System.out.println(pre[pre.length-1]);
     }
 }
